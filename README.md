890a843c6c977febb8a709f0c00c9580fdd8624f
dinoknowsbetter <dino@dd9.dev>
2022-05-09 17:39:21 -0400

    Addition of sales tax calculation endpoint

--- README.md
+++ README.md
@@ -1,19 +1,32 @@
-# TaxHype Node.js Library
+<h1 style="text-align:center">
+TaxHype Node.JS Server Library
+</h1>
 
-[![Version](https://badge.fury.io/js/taxhype.svg)](https://www.npmjs.org/package/taxhype)
-[![Downloads](https://img.shields.io/npm/dm/taxhype.svg)](https://www.npmjs.com/package/taxhype)
+![](https://img.shields.io/github/stars/hyperteksolutions/taxhype.svg)
+![](https://img.shields.io/github/forks/hyperteksolutions/taxhype.svg)
+![](https://img.shields.io/github/tag/hyperteksolutions/taxhype.svg)
+![](https://img.shields.io/github/release/hyperteksolutions/taxhype.svg)
+![](https://img.shields.io/github/issues/hyperteksolutions/taxhype.svg)
+
+---
+
+[![Npm package version](https://badgen.net/npm/v/taxhype)](https://npmjs.com/package/taxhype)
+[![NPM Package Downloads](https://badgen.net/npm/dt/taxhype)](https://npmjs.com/package/taxhype)
 [![Try on RunKit](https://badge.runkitcdn.com/taxhype.svg)](https://runkit.com/npm/taxhype)
-[![License](https://img.shields.io/github/license/Hypertek-Solutions/taxhype)](https://github.com/Hypertek-Solutions/taxhype/blob/master/LICENSE)
+[![License](https://img.shields.io/github/license/hyperteksolutions/taxhype)](https://github.com/Hypertek-Solutions/taxhype/blob/master/LICENSE)
+[![Dashboard](https://img.shields.io/website-up-down-green-red/http/shields.io.svg)](https://taxhype.com/)
 
-The TaxHype Node library provides easy access to TaxHype APIs for applications written in server-side JavaScript.
+---
+
+The TaxHype Node library allows for easy integration of TaxHype API's for applications written in server-side JavaScript.
 
 ## Documentation
 
-See the [API Docs](https://taxhype.com/api/docs)
+See the [API Docs](https://taxhype.dev)
 
 ## Requirements
 
-Node 8, 10 or higher.
+Node 16 or higher.
 
 ## Installation
 
@@ -27,9 +40,12 @@ yarn add taxhype
 
 ## Usage
 
-The package requires configuration with your API key and Secret Key, which are available in the [TaxHype Dashboard](https://dashboard.taxhype.com/).
+The package requires configuration with your API key and Secret Key, which are available in the [TaxHype Dashboard](https://dashboard.taxhype.app/).
 
 ```javascript
-const taxhype = require("taxhype");
-const client = new taxhype("API_KEY", "SECRET_KEY");
+const taxhype = require("taxhype")("API_KEY", "API_SECRET");
 ```
+
+## Questions / Suggestions
+
+For all questions and suggestions, please create a new issue [here](https://github.com/hyperteksolutions/taxhype/issues/new) or contact [contact@hypertek.dev](mailto:contact@hypertek.dev).
--- src/index.js
+++ src/index.js
@@ -1,37 +1,74 @@
 const axios = require("axios");
 
-module.exports = class TaxHype {
-  constructor(api_key, api_secret) {
-    this.api_key = api_key;
-    this.api_secret = api_secret;
+class TaxHype {
+  constructor(key, secret) {
+    this.key = key;
+    this.secret = secret;
     this.instance = axios.create({
-      baseURL: "https://api.taxhype.com/",
+      baseURL: "https://api.taxhype.com",
       timeout: 15000,
-      headers: { key: this.api_key, secret: this.api_secret },
+      headers: { key: this.key, secret: this.secret },
     });
   }
   getAccount = async () => {
-    if (this.api_key === "" || this.api_secret === "") {
-      return Promise.reject("API key and secret are required");
-    } else {
-      try {
-        let x = await this.instance.get("/client/getAccount");
-        return Promise.resolve(x.data);
-      } catch (error) {
-        return Promise.reject(error);
-      }
-    }
+    let x = await this.instance.get("/client/getAccount");
+    return Promise.resolve(x.data);
   };
+
   nexusStatus = async () => {
-    if (this.api_key === "" || this.api_secret === "") {
-      return Promise.reject("API key and secret are required");
-    } else {
-      try {
-        let x = await this.instance.get("/client/nexusStatus");
-        return Promise.resolve(x.data);
-      } catch (error) {
-        return Promise.reject(error);
-      }
-    }
+    let x = await this.instance.get("/client/nexusStatus");
+    return Promise.resolve(x.data);
   };
+
+  calculateTax = async (amount, zipCode) => {
+    let x = await this.instance.post("/client/nexusStatus", {
+      amount,
+      zipCode,
+    });
+    return Promise.resolve(x.data);
+  };
+}
+
+// module.exports = class TaxHype {
+//   constructor(api_key, api_secret) {
+//     this.api_key = api_key;
+//     this.api_secret = api_secret;
+//     this.instance = axios.create({
+//       baseURL: "https://api.taxhype.com/",
+//       timeout: 15000,
+//       headers: { key: this.api_key, secret: this.api_secret },
+//     });
+//   }
+//   getAccount = async () => {
+//     if (this.api_key === "" || this.api_secret === "") {
+//       return Promise.reject("API key and secret are required");
+//     } else {
+//       try {
+//         let x = await this.instance.get("/client/getAccount");
+//         return Promise.resolve(x.data);
+//       } catch (error) {
+//         return Promise.reject(error);
+//       }
+//     }
+//   };
+//   nexusStatus = async () => {
+//     if (this.api_key === "" || this.api_secret === "") {
+//       return Promise.reject("API key and secret are required");
+//     } else {
+//       try {
+//         let x = await this.instance.get("/client/nexusStatus");
+//         return Promise.resolve(x.data);
+//       } catch (error) {
+//         return Promise.reject(error);
+//       }
+//     }
+//   };
+// };
+
+var _old = TaxHype;
+TaxHype = function (...args) {
+  return new _old(...args);
 };
+
+module.exports = TaxHype;
+module.exports.default = TaxHype;
